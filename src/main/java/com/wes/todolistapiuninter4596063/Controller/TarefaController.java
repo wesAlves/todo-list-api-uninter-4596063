3a95b922feefc1e6900f4092e968b0481769d43f
Wes <wal2.designer@gmail.com>
2025-08-15 17:19:07 +0100

    Adicionando endpoint para buscar tarefa por ID no TarefaController.

--- src/main/java/com/wes/todolistapiuninter4596063/Controller/TarefaController.java
+++ src/main/java/com/wes/todolistapiuninter4596063/Controller/TarefaController.java
@@ -4,11 +4,10 @@ import com.wes.todolistapiuninter4596063.Model.Tarefa;
 import com.wes.todolistapiuninter4596063.Repository.TarefaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/tarefas")
@@ -21,4 +20,13 @@ public class TarefaController {
         List<Tarefa> tasks = tarefaRepository.findAll();
         return ResponseEntity.ok(tasks);
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Tarefa> getTaskById(@PathVariable Long id) {
+        return tarefaRepository.findById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+
 }
