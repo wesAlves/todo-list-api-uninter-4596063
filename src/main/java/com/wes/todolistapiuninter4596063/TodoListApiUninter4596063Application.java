2b45500c6978857f6441c64680a9b5531ce425e2
Wes <wal2.designer@gmail.com>
2025-08-15 16:34:56 +0100

    Iniciando o projeto e configurando o banco h2

---
+++ src/main/java/com/wes/todolistapiuninter4596063/TodoListApiUninter4596063Application.java
@@ -0,0 +1,13 @@
+package com.wes.todolistapiuninter4596063;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoListApiUninter4596063Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodoListApiUninter4596063Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/wes/todolistapiuninter4596063/TodoListApiUninter4596063ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wes.todolistapiuninter4596063;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoListApiUninter4596063ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
